96bdab58f12c0a3937c8d27031d18fa413c2e0e7
victor dominguez <victodom@ucm.es>
2018-04-17 12:55:38 +0200

    index log in modified for any  errors

--- code/login.php
+++ code/login.php
@@ -1,3 +1,28 @@
 <?php
-echo "holaaaa!!!"
+$db= mysqli_connect("localhost","root","","ett_laderas");
+
+
+$use=$_POST['user'];
+$pass=$_POST['password'];
+$encontrado=false;
+
+
+$result=mysqli_query($db,"select *from users ");
+while (($fila = mysqli_fetch_array($result))!=NULL){
+
+    if ($use==$fila['iduser'] && $pass== $fila['password']) $encontrado=true;
+
+
+}
+
+if (!$encontrado){
+    include 'alert.html';
+   require ('index.html');
+    header("refresh: 2; url=http://localhost/ett%20laderas/code/");
+
+}else{
+    echo "hola";
+}
+
+
 ?>
