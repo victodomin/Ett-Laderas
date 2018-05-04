6b4e86657c26e854e4b9e0e0278bcb90b63cd678
victor <victodom@ucm.es>
2018-05-04 14:11:34 +0200

    datbase modified

--- database/ett_laderas.sql
+++ database/ett_laderas.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 17, 2018 at 02:15 AM
+-- Generation Time: May 04, 2018 at 02:10 PM
 -- Server version: 10.1.30-MariaDB
 -- PHP Version: 7.2.2
 
@@ -25,47 +25,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `clients`
---
-
-CREATE TABLE `clients` (
-  `idclient` varchar(8) NOT NULL,
-  `name` varchar(8) NOT NULL,
-  `location` varchar(15) NOT NULL,
-  `sector` varchar(8) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `employees`
---
-
-CREATE TABLE `employees` (
-  `dni` varchar(7) NOT NULL,
-  `socialsecurity` int(8) NOT NULL,
-  `numberaccount` varchar(15) NOT NULL,
-  `name` varchar(8) NOT NULL,
-  `surname` varchar(8) NOT NULL,
-  `email` varchar(8) NOT NULL,
-  `residence` varchar(15) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `hiredemployees`
---
-
-CREATE TABLE `hiredemployees` (
-  `dni` varchar(7) NOT NULL,
-  `idclient` varchar(8) NOT NULL,
-  `idoffer` varchar(8) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `joboffers`
 --
 
@@ -78,68 +37,17 @@ CREATE TABLE `joboffers` (
   `description` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
-
 --
--- Table structure for table `payment`
+-- Dumping data for table `joboffers`
 --
 
-CREATE TABLE `payment` (
-  `idpayment` varchar(8) NOT NULL,
-  `dni` varchar(7) NOT NULL,
-  `idclient` varchar(8) NOT NULL,
-  `cuantity` int(6) NOT NULL,
-  `date` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `trained`
---
-
-CREATE TABLE `trained` (
-  `dni` varchar(7) NOT NULL,
-  `sector` varchar(8) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
-
-CREATE TABLE `users` (
-  `iduser` varchar(8) NOT NULL,
-  `password` varchar(12) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+INSERT INTO `joboffers` (`idoffer`, `idclient`, `sector`, `duration`, `salary`, `description`) VALUES
+('7788', '0000', 'IT', 65, 2400, 'Hi programmers needed to hack nsa'),
+('7777', '0000', 'TEC', 31, 1400, 'It engineers needed for developing a new app');
 
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `clients`
---
-ALTER TABLE `clients`
-  ADD PRIMARY KEY (`sector`),
-  ADD UNIQUE KEY `idclient` (`idclient`);
-
---
--- Indexes for table `employees`
---
-ALTER TABLE `employees`
-  ADD PRIMARY KEY (`dni`),
-  ADD UNIQUE KEY `dni` (`dni`);
-
---
--- Indexes for table `hiredemployees`
---
-ALTER TABLE `hiredemployees`
-  ADD PRIMARY KEY (`idclient`,`idoffer`),
-  ADD UNIQUE KEY `dni` (`dni`),
-  ADD UNIQUE KEY `dni_2` (`dni`),
-  ADD KEY `idoffer` (`idoffer`);
 
 --
 -- Indexes for table `joboffers`
@@ -150,38 +58,8 @@ ALTER TABLE `joboffers`
   ADD KEY `sector` (`sector`);
 
 --
--- Indexes for table `payment`
---
-ALTER TABLE `payment`
-  ADD PRIMARY KEY (`dni`,`idclient`),
-  ADD UNIQUE KEY `idpayment` (`idpayment`),
-  ADD KEY `payment_ibfk_2` (`idclient`);
-
---
--- Indexes for table `trained`
---
-ALTER TABLE `trained`
-  ADD PRIMARY KEY (`sector`),
-  ADD UNIQUE KEY `dni` (`dni`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`iduser`),
-  ADD UNIQUE KEY `iduser` (`iduser`);
-
---
 -- Constraints for dumped tables
 --
-
---
--- Constraints for table `hiredemployees`
---
-ALTER TABLE `hiredemployees`
-  ADD CONSTRAINT `hiredemployees_ibfk_1` FOREIGN KEY (`dni`) REFERENCES `employees` (`dni`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `hiredemployees_ibfk_2` FOREIGN KEY (`idclient`) REFERENCES `clients` (`idclient`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `hiredemployees_ibfk_3` FOREIGN KEY (`idoffer`) REFERENCES `joboffers` (`idoffer`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `joboffers`
@@ -189,20 +67,6 @@ ALTER TABLE `hiredemployees`
 ALTER TABLE `joboffers`
   ADD CONSTRAINT `joboffers_ibfk_1` FOREIGN KEY (`idclient`) REFERENCES `clients` (`idclient`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `joboffers_ibfk_2` FOREIGN KEY (`sector`) REFERENCES `clients` (`sector`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
---
--- Constraints for table `payment`
---
-ALTER TABLE `payment`
-  ADD CONSTRAINT `payment_ibfk_1` FOREIGN KEY (`dni`) REFERENCES `employees` (`dni`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `payment_ibfk_2` FOREIGN KEY (`idclient`) REFERENCES `joboffers` (`idclient`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
---
--- Constraints for table `trained`
---
-ALTER TABLE `trained`
-  ADD CONSTRAINT `trained_ibfk_1` FOREIGN KEY (`dni`) REFERENCES `employees` (`dni`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `trained_ibfk_2` FOREIGN KEY (`sector`) REFERENCES `clients` (`sector`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
